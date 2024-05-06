8dbbe4f560c00f325f6d4b095b817bdee8795938
TransformersAli58 <146876301+TransformersAli58@users.noreply.github.com>
2024-05-06 21:02:17 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+url = 'https://www.example.com'
+
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'html.parser')
+basliklar = soup.find_all('h1')
+for baslik in basliklar:
+    print(baslik.text)
